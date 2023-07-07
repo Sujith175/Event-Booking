0c9a040fcfbf9e34bb7d0d146e7a240118eb3688
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2023-07-07 22:30:37 +0530

    feat:json web token

--- graphQl/resolvers/auth.js
+++ graphQl/resolvers/auth.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const User = require("../../models/user");
 
 module.exports = {
@@ -20,6 +20,26 @@ module.exports = {
       return { ...result._doc, password: null, _id: result.id };
     } catch (err) {
       throw err;
+    }
+  },
+  login: async ({ email, password }) => {
+    try {
+      const existingUser = await User.findOne({ email: email });
+      if (!existingUser) {
+        throw new Error("User Not Registered with this Email");
+      }
+      const isEqual = await bcrypt.compare(password, existingUser.password);
+      if (!isEqual) {
+        throw new Error("Password Doesn't Match");
+      }
+      const token = jwt.sign(
+        { userId: existingUser.id, email: existingUser.email },
+        "somerandomprivatekey",
+        { expiresIn: "1h" }
+      );
+      return { userId: existingUser.id, token: token, tokenExpiration: 1 };
+    } catch (error) {
+      throw error;
     }
   },
 };
